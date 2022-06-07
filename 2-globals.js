c39b4b7a3dee60ef3dde951f2838a53fd04052a4
hengzhengping <hengzhengping@gmail.com>
2022-06-07 19:53:22 +0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,15 @@
+// GOBALS - NO WINDOW !!!
+
+// __dirname    - path to current directory
+// __filename   - file name
+// require      - function to use modules (CommonJS)
+// module       - info about current module (file)
+// process      - info about env where the program is being executed
+
+// type clear in terminal to clear terminal
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade');
+
+sayHi('Susan')
+sayHi(names.john)
+sayHi(names.peter)
